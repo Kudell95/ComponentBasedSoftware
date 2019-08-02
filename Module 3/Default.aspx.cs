eafd597cb2b706dccb83b2d2e1a0e08bd5227fe5
Matt <kudell95@hotmail.com>
2019-08-02 12:08:18 +1000

    added class

--- Module 3/Default.aspx.cs
+++ Module 3/Default.aspx.cs
@@ -7,6 +7,8 @@ using System.Web.UI.WebControls;
 
 public partial class CIS2003_Module_3_Default : Page
 {
+
+    public Person currentCustomer = new Person();
     protected void Page_Load(object sender, EventArgs e)
     {
        
