5a60c86f42f57d58110999bcf9c701eb22aebaf8
Matt <kudell95@hotmail.com>
2019-08-15 11:48:13 +1000

    customer class

--- Kudell0061060303/App_Code/Customer.cs
+++ Kudell0061060303/App_Code/Customer.cs
@@ -8,12 +8,48 @@ using System.Web;
 /// </summary>
 public class Customer
 {
+    
+
+    private string Name, Address;
+    private int Age, Id;
+    
     public Customer()
     {
         //
         // TODO: Add constructor logic here
         //
     }
+
+    public Customer(string name, int age, string address)
+    {
+        Name = name;
+        Age = age;
+        Address = address;
+
+    }
+
+    //Getters-------------------------------|
+    public string GetName() { return Name; }
+    public string GetAddress() { return Address; }
+    public int GetAge() { return Age; }
+    public int GetID() { return Id; }
+    
+
+    //Setters ------------------------------|
+    public void SetName(string name) { Name = name; }
+    public void SetAddress(string address) { Address = address; }
+    public void SetAge(int age) { Age = age; }
+    public void GenerateID()
+    {
+        //create unique id;
+    }
+         
+
+
+
+
+
+
 
 
 }
